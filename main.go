5f07c5b08890c4b52c387513a8a15abb7f1b957e
Ilia Talebzadeh <iliasins82@gmail.com>
2021-07-23 05:01:22 -0700

    init

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Item struct {
+	UID   string  `json:"UID"`
+	Name  string  `json:"Name"`
+	Desc  string  `json:"Desc"`
+	Price float64 `json:"Price"`
+}
+
+var inventory []Item
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "EndPoint called: homePage()")
+}
+
+func deleteItem(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	_deleteItemAtUid(params["uid"])
+
+	json.NewEncoder(w).Encode(inventory)
+}
+
+func _deleteItemAtUid(uid string) {
+	for index, item := range inventory {
+		if item.UID == uid {
+			// Delete item from slice
+			inventory = append(inventory[:index], inventory[index+1:]...)
+			break
+		}
+	}
+}
+
+func updateItem(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var item Item
+	_ = json.NewDecoder(r.Body).Decode(&item)
+}
+
+func getInventory(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Println("Function callled: getInventory()")
+
+	json.NewEncoder(w).Encode(inventory)
+}
+
+func createItem(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var item Item
+	_ = json.NewDecoder(r.Body).Decode(&item)
+
+	inventory = append(inventory, item)
+
+	json.NewEncoder(w).Encode(item)
+}
+
+func handleRequest() {
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", homePage).Methods("GET")
+	router.HandleFunc("/inventory", getInventory).Methods("GET")
+	router.HandleFunc("/inventory", createItem).Methods("POST")
+	router.HandleFunc("/inventory/{uid}", deleteItem).Methods("DELETE")
+	router.HandleFunc("/inventory/{uid}", updateItem).Methods("PUT")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func main() {
+	inventory = append(inventory, Item{
+		UID:   "0",
+		Name:  "Cheese",
+		Desc:  "a fine block of Cheese",
+		Price: 4.99,
+	})
+	inventory = append(inventory, Item{
+		UID:   "1",
+		Name:  "Milk",
+		Desc:  "A jug of milk",
+		Price: 3.25,
+	})
+	handleRequest()
+}
